80907f443e9392e3a8bbb69b1ca7267f60ff05c6
HbC <hbcsoft@outlook.com>
2020-10-05 23:31:22 +0100

    WpfTaskPane

---
+++ samples/Wpf4ExcelSample/MyCustomRibbon.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Runtime.InteropServices
+Imports ExcelDna.Integration.CustomUI
+Imports just4Excel.Core.TaskPane
+
+<ComVisible(True)>
+Public Class MyCustomRibbon : Inherits ExcelRibbon
+
+
+
+    ''' <summary>
+    ''' Use this function to return the custom ribbon XML.
+    ''' </summary>
+    ''' <param name="RibbonID"></param>
+    ''' <returns></returns>
+    Public Overrides Function GetCustomUI(RibbonID As String) As String
+
+        Return "
+    <customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui'>
+      <ribbon>
+        <tabs>
+          <tab id='myTab' label='My Tab' visible='true'>
+            <group id='myGroup' label='My Group' visible='true'>
+              <button id='myButton' label='My Button' size='large' imageMso='HappyFace' onAction='OnMyButton_Action' visible='true'/>
+            </group >
+          </tab>
+        </tabs>
+      </ribbon>
+    </customUI>"
+
+    End Function
+
+
+
+
+    ''' <summary>
+    ''' This procedure needs to have the same name as the property onAction.
+    ''' </summary>
+    ''' <param name="control"></param>
+    Public Sub OnMyButton_Action(control As IRibbonControl)
+
+
+        If myTaskPane Is Nothing Then
+            myTaskPane = myUserControl.ShowInTaskPane("My user control")
+        Else
+            myTaskPane.Visible = True
+        End If
+
+
+    End Sub
+
+
+    Private myUserControl As New MyUserControl
+    Private myTaskPane As WpfTaskPane
+
+
+End Class
---
+++ samples/Wpf4ExcelSample/README.md
@@ -0,0 +1,15 @@
+# Wpf4ExcelSample
+Sample for usage of WPF in Excel
+
+
+1. Create a new WPF User Contol Library project in Visual Basic, C# or F#.
+1. Install Excel-DNA
+   1. PM> Install-Package ExcelDna.AddIn
+1. Add reference to just4Excel.Core
+4. The file MyCustomRibbon.vb adds a custom ribbon and loads a custom WPF
+
+
+
+
+
+
